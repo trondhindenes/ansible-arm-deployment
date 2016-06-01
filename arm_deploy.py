77411455a2b5ee5ee7295e11077fdad60c5ee3f9
Trond Hindenes <trond@hindenes.com>
2016-06-01 06:04:43 +0200

    json load template json

--- arm_deploy.py
+++ arm_deploy.py
@@ -86,13 +86,17 @@ import sys
 import time
 import requests
 import os.path
+import ConfigParser
+from os.path import expanduser
+import json
+
 
 HAS_ARM = False
 
 try:
-    from azure.mgmt.resource.resources.models import ResourceGroup
+    from azure.mgmt.resource.resources.models import ResourceGroup, DeploymentProperties, DeploymentMode
     from azure.mgmt.resource.resources import ResourceManagementClient, ResourceManagementClientConfiguration
-    from azure.common.credentials import ServicePrincipalCredentials
+    from azure.common.credentials import ServicePrincipalCredentials, UserPassCredentials
     HAS_ARM = True
 except ImportError:
     pass
@@ -112,10 +116,13 @@ def main():
 
     module = AnsibleModule(
         argument_spec = dict(
-            client_id = dict(required=True),
-            client_secret = dict(required=True),
-            tenant_id = dict(required=True),
-            subscription_id = dict(required=True),
+            client_id = dict(required=False),
+            client_secret = dict(required=False),
+            tenant_id = dict(required=False),
+            subscription_id = dict(required=False),
+            profile= dict(required=False),
+            ad_user = dict(required=False),
+            password = dict(required=False),
             template_src_json = dict(required=True),
             param_src_json = dict(),
             resource_group_name = dict(required=True),
@@ -126,15 +133,25 @@ def main():
         supports_check_mode = False,
     )
     
-    
+    creds_params = {}
+    print( module.params.get('client_id'))
     if not HAS_ARM:
         module.fail_json(msg='azure python sdk required for this module')
 
-
-    client_id = module.params.get('client_id')
-    client_secret = module.params.get('client_secret')
-    tenant_id = module.params.get('tenant_id')
-    subscription_id = module.params.get('subscription_id')
+    if module.params['client_id']:
+        creds_params['client_id'] = module.params.get('client_id')
+    if module.params['client_secret']:
+        creds_params['client_secret'] = module.params.get('client_secret')
+    if module.params['tenant_id']:
+        creds_params['tenant_id'] = module.params.get('tenant_id')
+    if module.params['subscription_id']:
+        creds_params['subscription_id'] = module.params.get('subscription_id')
+    if module.params['profile']:
+        profile = module.params.get('profile')
+    if module.params['ad_user']:
+        creds_params['ad_user'] = module.params.get('ad_user')
+    if module.params['password']:
+        creds_params['password'] = module.params.get('password')
     template_src_json = module.params.get('template_src_json')
     if module.params['param_src_json']:
         param_src_json = module.params.get('param_src_json')
@@ -147,18 +164,38 @@ def main():
     else:
         resource_group_location = 'none'
     #try:
-    endpoint='https://login.microsoftonline.com/' + tenant_id + '/oauth2/token'
+    
     #authenticate to azure
-    auth_token = get_token_from_client_credentials(
-        endpoint=endpoint,
-        client_id=client_id,
-        client_secret=client_secret,
-    )
     
-    creds = ServicePrincipalCredentials(client_id=client_id, secret=client_secret, tenant=tenant_id)
+    if profile:
+      path = expanduser("~/.azure/credentials")
+      try:
+            config = ConfigParser.SafeConfigParser()
+            config.read(path)
+      except Exception as exc:
+          self.fail("Failed to access {0}. Check that the file exists and you have read access. {1}".format(path, str(exc)))
+    if not config.has_section(profile):
+        self.fail("Config file does not appear to have section " + profile)
+    for key, val in config.items(profile):
+        creds_params[key] = val
+
     
+    if 'client_id' in creds_params and 'client_secret' in creds_params:
+      endpoint='https://login.microsoftonline.com/' + creds_params['tenant_id'] + '/oauth2/token'
+      auth_token = get_token_from_client_credentials(
+          endpoint=endpoint,
+          client_id=creds_params['client_id'],
+          client_secret=creds_params['client_secret'],
+      )
+      creds = ServicePrincipalCredentials(client_id=creds_params['client_id'], secret=creds_params['client_secret'],
+                                          tenant=creds_params['tenant_id'])
+
+    elif 'ad_user' in creds_params and 'password' in creds_params:
+      creds = UserPassCredentials(creds_params['ad_user'], creds_params['password'])
+    
+          
     #construct resource client 
-    config = ResourceManagementClientConfiguration(creds, subscription_id)
+    config = ResourceManagementClientConfiguration(creds, creds_params['subscription_id'])
     resource_client = ResourceManagementClient(config)
     
     #Check rg
@@ -178,44 +215,57 @@ def main():
         result = resource_client.resource_groups.create_or_update(
             resource_group_name,
             ResourceGroup(
-            location=resource_group_location,
+                location=resource_group_location,
             ),
         )
     
     #read template file and params file
-    templatefile = open(template_src_json)
+    templatefile = open(template_src_json, 'r')
     template = templatefile.read()
     templatefile.close()
+    #templatedata = "".join(line.rstrip() for line in template)
+    templatedata = json.loads(template)
     
     #If param file doesnt exist, use an empty json thingy
     if (param_src_json == 'none'):
-      param = '{}'
+        paramdata = None
     elif (os.path.isfile(param_src_json)):
-      paramfile = open(param_src_json)
-      param = paramfile.read()
+      paramfile = open(param_src_json, 'rU')
+      param = paramfile.readlines()
       paramfile.close()
+      paramdata = "".join(line.rstrip() for line in param)
     else:
-      param = '{}'
+        paramdata = None
     
-    
+    module.log(templatedata)
+
     #create deployment props
-    properties = azure.mgmt.resource.resourcemanagement.DeploymentProperties(
+
+    '''
+    properties = DeploymentProperties(
       mode="incremental",
       template=template,
       parameters=param
     )
     deploy_parameter = azure.mgmt.resource.Deployment()
     deploy_parameter.properties=properties
-    
+    '''
+
     #invoke the thing
     result = resource_client.deployments.create_or_update(
-    resource_group_name=resource_group_name,
-    deployment_name=deployment_name,
-    parameters=deploy_parameter
+        resource_group_name,
+        deployment_name,
+        properties=DeploymentProperties(
+            mode=DeploymentMode.incremental,
+            template=templatedata,
+            parameters=paramdata)
     )
 
+    if result and result._exception and result._exception.message:
+        module.fail_json(msg=result._exception.message)
+
     while True:
-        status = resource_client.deployments.get(resource_group_name,deployment_name)
+        status = resource_client.deployments.get(resource_group_name, deployment_name)
         time.sleep(1)
         print(status.deployment.properties.provisioning_state)
         if status.deployment.properties.provisioning_state == 'Succeeded':
